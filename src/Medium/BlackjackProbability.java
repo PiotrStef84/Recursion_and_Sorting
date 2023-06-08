bbb25ca0b04502514c02a2fd007447d8a531da38
Piotr <xmestre@wp.pl>
2023-06-08 16:51:28 +0100

    Started blackjack probability challenge

---
+++ src/Medium/BlackjackProbability.java
@@ -0,0 +1,42 @@
+package Medium;
+
+import java.util.Scanner;
+
+public class BlackjackProbability {
+
+
+    /* Write a function that takes in a target value as a dealer's startingHand value and returns the
+    * probability that the dealer will bust (go over target value before standing). The target value will
+    * always be positive integer, and the startingHand value will always be a positive integer that's smaller
+    * or equal to the target value.
+    *
+    * For simplicity, you can assume that the dealer has an infinite deck of cards and that each card has a
+    * value between 1 and 10 inclusive. The likelihood of drawing a card of any value is always the same,
+    * regardless of previous draws.
+    *
+    * Your solution should be rounded to 3 decimal places and to the nearest value. For example, a probability
+    * of 0.314159 would be rounded to 0.314, while a probability of 0.1337 would be rounded to 0.134 */
+
+    // O(t - s) time | O(t - s) space where t is a target value, s is a startingHand,
+    // as t - s  represents the maximum number of calculations to be made
+    public static void main(String[] args) {
+
+        System.out.println("Enter starting hand:");
+        Scanner scan = new Scanner(System.in);
+
+        int startingHand = scan.nextInt();
+
+        System.out.println("You entered: " + startingHand);
+
+        int target = 21;
+
+        float result = blackjackProbability(target, startingHand);
+        System.out.println(result);
+
+    }
+
+    private static float blackjackProbability(int target, int startingHand) {
+
+        return 20.22f;
+    }
+}
