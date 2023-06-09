738374f1efc1138d3b40a11d4aba890730159a04
Piotr <xmestre@wp.pl>
2023-06-09 20:31:59 +0100

    Bubble Sort

---
+++ src/Sorting/Easy/BubbleSort.java
@@ -0,0 +1,11 @@
+package Sorting.Easy;
+
+public class BubbleSort {
+
+    /* Write a function that takes in an array of integers and returns a sorted version of that array. Use the
+    * Bubble Sort algorithm to sort the array*/
+
+    public static void main(String[] args) {
+
+    }
+}
